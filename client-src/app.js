601e66c2b2fd73020d82134f13cd9c69df9fa578
Yaroslav Dashkevich <y.dashkevich@amber.software>
2016-12-08 16:51:46 +0700

    Make map to remember it's state

--- client-src/app.js
+++ client-src/app.js
@@ -5,11 +5,13 @@ require('./app.less');
 import { getOsmTile } from './tile';
 import { findYaTile } from './ya';
 
+import * as request from 'superagent';
+
 // init map
 var southWest = L.latLng(30, 10),
     northEast = L.latLng(80, 200),
     bounds = L.latLngBounds(southWest, northEast);
-var mapParams =
+var mapParams = JSON.parse(localStorage.getItem('map-state')) ||
 {
   lat: 54.86587703802659,
   lng: 83.08142423629762,
@@ -47,15 +49,35 @@ Map.on(
   }
 );
 
+Map.on(
+  'moveend',
+  () =>
+  {
+    var c = Map.getCenter();
+    localStorage.setItem('map-state', JSON.stringify({lat: c.lat, lng: c.lng, zoom: Map.getZoom()}));
+  }
+);
+
+Map.on(
+  'zomeend',
+  () =>
+  {
+    var c = Map.getCenter();
+    localStorage.setItem('map-state', JSON.stringify({lat: c.lat, lng: c.lng, zoom: Map.getZoom()}));
+  }
+);
+
 function getPopupContent(latLng, zoom, osmTile, yaTile)
 {
   var osmLink = `http://c.tile.openstreetmap.org/${zoom}/${osmTile.x}/${osmTile.y}.png`;
-  var yaLink, yaText, trafficLink, trafficText
+  var yaLink, yaText, trafficLink, trafficText, jsonLink, jsonText;
 
   yaLink = `https://vec04.maps.yandex.net/tiles?l=map&v=4.130.1&x=${yaTile.x}&y=${yaTile.y}&z=${zoom}&scale=1`;
   yaText = `<a href="${yaLink}" target="_blank">${yaLink}</a>`;
   trafficLink = `https://jgo.maps.yandex.net/1.1/tiles?trf&l=trf,trfe&lang=ru_RU&x=${yaTile.x}&y=${yaTile.y}&z=${zoom}&scale=1&tm=${unixNow()}`;
   trafficText = `<a href="${trafficLink}" target="_blank">${trafficLink}</a>`;
+  jsonLink = `https://jgo.maps.yandex.net/1.1/tiles?trf&l=trj,trje&lang=ru_RU&x=${yaTile.x}&y=${yaTile.y}&z=${zoom}&scale=1&tm=${unixNow()}`;
+  jsonText = `<a href="${jsonLink}" target="_blank">${jsonLink}</a>`;
 
   var out =
   `<h5>Coordinates:</h5>
@@ -68,6 +90,8 @@ function getPopupContent(latLng, zoom, osmTile, yaTile)
   ${yaText}
   <h5>Traffic tile</h5>
   ${trafficText}
+  <h5>Traffic json</h5>
+  ${jsonText}
   `;
 
   return out;
@@ -77,3 +101,18 @@ function unixNow()
 {
   return Math.round( Date.now() / 1000 );
 }
+
+// var stamp = 0;
+
+// function fetchTraffic(link)
+// {
+//   request
+//   .get(link)
+//   .end(
+//     (err, res) =>
+//     {
+//       console.log(err, res);
+
+//     }
+//   );
+// }
